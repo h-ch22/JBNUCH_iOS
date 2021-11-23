5a24b94e760e75789cd66059417d7c2595b47d11
h-ch22 <ckdwls9460@naver.com>
2021-11-23 15:07:17 +0900

    Initial Commit

---
+++ JBNU-CH/JBNU_CHApp.swift
@@ -0,0 +1,17 @@
+//
+//  JBNU_CHApp.swift
+//  JBNU-CH
+//
+//  Created by 하창진 on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JBNU_CHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
